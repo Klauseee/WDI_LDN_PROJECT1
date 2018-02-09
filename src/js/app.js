9736b15c9ae7431c62c834c1fc067f3c258a5946
Nic Wilson <nicholaswilson3010@gmail.com>
2018-02-09 14:55:14 +0000

    Basic layout and initial code completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,65 @@
-[10,20,30].forEach(num => console.log(num));
+function init() {
+
+  // GLOBAL VARIABLES
+  const gridIndex = [];
+  const xCommands = [];
+  const moves = {
+    'move forward': moveForward,
+    'turn right': turnRight,
+    'turn left': turnLeft
+  };
+  let numCommands = 1;
+
+
+  // GET DOM ELEMENTS
+  const $grid = $('.grid');
+  const $addMove = $('.add-move');
+  const $moves = $('.moves');
+  const $execute = $('.execute');
+
+  // GLOBAL FUNCTIONS
+  function setGrid(n) {
+    for (var i = 0; i < n; i++) {
+      gridIndex.push(i);
+    }
+    return gridIndex;
+  }
+
+  function moveForward() {
+
+  }
+
+  function turnRight() {
+
+  }
+
+  function turnLeft() {
+
+  }
+
+  // DOM FUNCTIONS
+
+  // CREATE GRID
+  setGrid(256);
+  gridIndex.forEach(() => {
+    $grid.append($('<div></div>'));
+  });
+
+  $addMove.on('click', () => {
+    numCommands ++;
+    const $newCommand = $('#first').clone();
+    $newCommand.attr('name', numCommands).val('').appendTo($moves);
+  });
+
+  $execute.on('click', () => {
+    const $commands = $('.command');
+    $commands.toArray().forEach((command) => {
+      xCommands.push(command.value);
+    });
+  });
+
+
+
+}
+
+$(document).ready(init);
